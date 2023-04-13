bd14d1d4d25081f294c2101184cb785e7ce78ea6
Nikki van Wijngaarden <90691202+Nikkivw@users.noreply.github.com>
2023-04-13 11:34:41 +0200

    update

--- TowerDefence/Assets/Assets/Scripts/EnemyWaveManager.cs
+++ TowerDefence/Assets/Assets/Scripts/EnemyWaveManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class EnemyWaveManager : MonoBehaviour
 {
     public GameObject enemyObject;
+    public GameObject Start_Path;
     private List<Vector2Int> pathBlocks;
     private GameObject enemyInstance;
     int nextPathblockIndex;
     void Start()
     {
-        enemyInstance = Instantiate(enemyObject, new Vector3(0, 0.2f, 0), Quaternion.identity);
+        enemyInstance = Instantiate(enemyObject, new Vector3(-1f, -0.5f, 5f), Quaternion.identity);
         nextPathblockIndex = 1;
     }
 
@@ -20,7 +21,7 @@ public class EnemyWaveManager : MonoBehaviour
         if (pathBlocks != null && pathBlocks.Count > 1)
         {
             Vector3 currentPos = enemyInstance.transform.position;
-            Vector3 nextPos = new Vector3(pathBlocks[nextPathblockIndex].x, 0.2f, pathBlocks[nextPathblockIndex].y);
+            Vector3 nextPos = new Vector3(pathBlocks[nextPathblockIndex].x, 0.4f, pathBlocks[nextPathblockIndex].y);
             enemyInstance.transform.position = Vector3.MoveTowards(currentPos, nextPos, Time.deltaTime);
             if (Vector3.Distance(currentPos,nextPos)< 0.05f)
             {
